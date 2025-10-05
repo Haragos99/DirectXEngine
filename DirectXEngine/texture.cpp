cdc1ce5cb5ac99abdb0b53381ef1e88c60b9cca1
Haragos99 <geri.haragos@gmail.com>
2025-10-05 10:30:02 +0200

    add simple color texture mode

--- DirectXEngine/texture.cpp
+++ DirectXEngine/texture.cpp
@@ -1,5 +1,6 @@
-#include "texture.h"
+﻿#include "texture.h"
 #include "../External/WICTextureLoader.h"
+#include <stdexcept>
 
 
 Texture::Texture(Microsoft::WRL::ComPtr<ID3D11Device> _device, Microsoft::WRL::ComPtr<ID3D11DeviceContext> _contex) : device(_device), contex(_contex)
@@ -37,4 +38,45 @@ void Texture::CreateSamplerState()
 	desc.MaxLOD = 1000.0f;
 
 	device->CreateSamplerState(&desc, m_SamplerState.GetAddressOf());
+}
+
+
+void Texture::CreateSolidColorTexture(const DirectX::XMFLOAT4& color)
+{
+    // Create a 1x1 texture with a single color
+    D3D11_TEXTURE2D_DESC desc = {};
+    desc.Width = 1;
+    desc.Height = 1;
+    desc.MipLevels = 1;
+    desc.ArraySize = 1;
+    desc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    desc.SampleDesc.Count = 1;
+    desc.Usage = D3D11_USAGE_IMMUTABLE;
+    desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+
+    // Convert color from float4 [0..1] → byte RGBA [0..255]
+    UINT rgba =
+        (UINT(color.w * 255.0f) << 24) |
+        (UINT(color.z * 255.0f) << 16) |
+        (UINT(color.y * 255.0f) << 8) |
+        (UINT(color.x * 255.0f));
+
+    D3D11_SUBRESOURCE_DATA initData = {};
+    initData.pSysMem = &rgba;
+    initData.SysMemPitch = sizeof(UINT);
+
+    Microsoft::WRL::ComPtr<ID3D11Texture2D> tex;
+    HRESULT hr = device->CreateTexture2D(&desc, &initData, tex.GetAddressOf());
+    if (FAILED(hr))
+        throw std::runtime_error("Failed to create solid color texture!");
+
+    // Create shader resource view
+    D3D11_SHADER_RESOURCE_VIEW_DESC srvDesc = {};
+    srvDesc.Format = desc.Format;
+    srvDesc.ViewDimension = D3D11_SRV_DIMENSION_TEXTURE2D;
+    srvDesc.Texture2D.MipLevels = 1;
+
+    hr = device->CreateShaderResourceView(tex.Get(), &srvDesc, m_DiffuseTexture.GetAddressOf());
+    if (FAILED(hr))
+        throw std::runtime_error("Failed to create SRV for solid color texture!");
 }
--- DirectXEngine/texture.h
+++ DirectXEngine/texture.h
@@ -2,7 +2,7 @@
 #include <d3d11.h>
 #include <wrl\client.h>
 #include <string>
-
+#include <DirectXMath.h>
 using Microsoft::WRL::ComPtr;
 class Texture
 {
@@ -12,6 +12,7 @@ public:
 	Texture(Microsoft::WRL::ComPtr<ID3D11Device> _device, Microsoft::WRL::ComPtr<ID3D11DeviceContext> _contex);
 	virtual ~Texture() = default;
 	void LoadTextureFromFile(std::wstring filename);
+	void CreateSolidColorTexture(const DirectX::XMFLOAT4& color);
 	// Bind texture sampler to pipeline
 	void Use();
 
